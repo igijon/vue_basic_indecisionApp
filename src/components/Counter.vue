5947a67ecc1d62af2a0b08be3a601e0aecce13c2
Inma Gijón <igijoninf@gmail.com>
2022-02-04 21:46:14 +0100

    Estado del componente y uso con Vue tools Chrome

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,10 +1,19 @@
 <template>
     <h2>Counter</h2>
-    <p>10</p>
+    <!-- En este ejemplo hemos visto cómo con las herramientas de desarrollo para Vue de Chrome podemos mantener el estado
+    y hacer modificaciones para probar-->
+    
+    <p>{{ counter }} <sup>2</sup> = {{ counter * counter }}</p>
 </template>
 
 <script>
 export default {
+    //name: 'Patito'
+    data() {
+        return {
+            counter: 5
+        }
+    }
 }
 </script>
 
