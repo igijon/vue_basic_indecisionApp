cfeaa0e80472267a08602417aa33ddf9b73d2f6c
Inma Gijón <igijoninf@gmail.com>
2022-02-04 20:17:36 +0100

    Creado nuevo componente y añadido a App

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  
+  <Counter />
 </template>
 
 <script>
 import Counter from "./components/Counter.vue";
+
+
 
 export default {
   name: 'App',
