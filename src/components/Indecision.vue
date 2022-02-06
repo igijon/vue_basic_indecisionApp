059361bc48229b09c3f12909a9bae648c52544bd
Inma Gijón <igijoninf@gmail.com>
2022-02-06 10:25:23 +0100

    La pregunta y respuesta sólo aparece cuando deben.

--- src/components/Indecision.vue
+++ src/components/Indecision.vue
@@ -5,7 +5,7 @@
       <input v-model="question" type="text" placeholder="Hazme una pregunta">
       <p>Recuerda terminar con un signo de interrogación (?)</p>
 
-      <div>
+      <div v-if="isValidQuestion">
           <h2>{{ question }}</h2>
           <h1>{{ answer }}</h1>
       </div>
@@ -19,7 +19,8 @@ export default {
         return {
             question: null,
             answer: null,
-            img: null
+            img: null,
+            isValidQuestion: false
         }
     },
     methods: {
@@ -35,7 +36,11 @@ export default {
     watch: {
         question ( value, oldValue ){
 
+            this.isValidQuestion = false
+
             if( !value.includes('?') ) return
+
+            this.isValidQuestion = true
             
             this.getAnswer()
 
