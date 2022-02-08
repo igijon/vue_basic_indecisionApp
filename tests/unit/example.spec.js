cebe9916363cca3170116e4fd1c7d20b839ef453
Inma Gijón <igijoninf@gmail.com>
2022-02-08 14:00:51 +0100

    Primera prueba. Resultado error

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,26 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+
+//Nombre que le damos al test-suite. Dentro vamos a tener una función.
+describe('Example Component', () => {
+  //Dentro de aquí haremos las pruebas asociadas a este componente
+  //Cada test es una prueba y su objetivo es hacer una evaluación pequeña
+  test( 'Debe ser mayor a 10', () => {
+    
+    //Esta función podría ser asíncrona en un momento determinado, aún no.
+
+    //Preparar
+    const value = 5  //Como es un ejemplo, esto va a ser el sujeto de pruebas
+
+    //Estímulo
+    value = value + 2
+
+    //Observar el resultado
+    if ( value > 10 ) {
+      //TODO: todo bien
+    } else {
+      throw `${ value } no es mayor a 10`
+    }
   })
 })
+
+//para ejecutar yarn test:unit
