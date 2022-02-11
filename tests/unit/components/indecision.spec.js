01477693ace3198f8686caad9e6614467d2ae051
Inma Gijón <igijoninf@gmail.com>
2022-02-11 20:01:42 +0100

    Spy: console.log llamado

--- tests/unit/components/indecision.spec.js
+++ tests/unit/components/indecision.spec.js
@@ -3,10 +3,12 @@ import Indecision from '@/components/Indecision'
 
 describe('Indecision component', () => {
 
-    let wrapper;
+    let wrapper
+    let clgSpy
 
     beforeEach(() => {
         wrapper = shallowMount( Indecision )
+        clgSpy = jest.spyOn(console, 'log') //Voy a espiar el objeto console y el método log
     })
 
     test('debe hacer match con el snapshot', () => {
@@ -14,7 +16,17 @@ describe('Indecision component', () => {
     })
 
     //Voy a definir algunas pruebas que quiero realizar
-    test('escribir en el input no debe disparar nada', () => {
+    test('escribir en el input no debe disparar nada', async () => {
+        //Comprobaremos que se dispara el console.log
+        //Tenemos un watcher pendiente del valor del question que está asociado al input
+        const input = wrapper.find('input') //Sólo hay uno
+        //Pongo el await para forzar a que se setee antes de seguir
+        await input.setValue('Hola mundo') //Le establezco el valor
+
+        //Al establecerle el valor, se habrá producido el console.log
+        //Para eso teng que crearme un espía (spy) 
+
+        expect( clgSpy ).toHaveBeenCalled()
 
     })
 
